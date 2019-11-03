65b7e2613f4bb4450a9fa6192b2a19feefde8423
AtsushiTokuoka <atsushi.tokuoka@gmail.com>
2019-11-03 13:13:48 +0900

    babel処理追加、jquery・vueライブラリ読み込み

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,9 +1,35 @@
+
+webpack = require("webpack");
+
 module.exports = {
     mode: "development",
     entry: {
-        application: './assets/js/main.js'
+        application: "./assets/js/main.js"
     },
     output: {
         filename: "bundle.js"
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                "@babel/preset-env"
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            Vue: ["vue/dist/vue.esm.js", "default"]
+        })
+    ]
 } 
