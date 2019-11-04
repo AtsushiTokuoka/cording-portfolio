73973791374e2ee80139d995cebce8156b20a7b5
AtsushiTokuoka <atsushi.tokuoka@gmail.com>
2019-11-04 13:18:27 +0900

    .vueコンポーネントを追加

---
+++ src/assets/js/main.js
@@ -0,0 +1,8 @@
+import ProductList from '../../component/product-list.vue';
+
+const app = new Vue({
+  el: '#app',
+  components: {
+    'product-list': ProductList
+  }
+});
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,35 +1,54 @@
-
-webpack = require("webpack");
+const webpack = require('webpack');
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
 
 module.exports = {
-    mode: "development",
-    entry: {
-        application: "./assets/js/main.js"
-    },
-    output: {
-        filename: "bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: [
-                    {
-                        loader: "babel-loader",
-                        options: {
-                            presets: [
-                                "@babel/preset-env"
-                            ]
-                        }
-                    }
-                ]
+  mode: 'development',
+  entry: {
+    js: './assets/js/main.js'
+  },
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
             }
+          }
         ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            $: "jquery",
-            Vue: ["vue/dist/vue.esm.js", "default"]
-        })
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: '../images/',
+              publicPath: function(path) {
+                return './assets/images/' + path;
+              }
+            }
+          }
+        ]
+      }
     ]
-} 
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      Vue: ['vue/dist/vue.esm.js', 'default']
+    }),
+    new VueLoaderPlugin()
+  ]
+};
