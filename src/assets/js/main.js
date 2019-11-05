cd3125f9c0edaf6e177ffbf21e9380e4ee811f09
tokuoka <tokuoka@ivp.co.jp>
2019-11-05 17:08:05 +0900

    header.vue テンプレートの作成

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,10 @@
+import Header from '../../component/common/header.vue'
 import ProductList from '../../component/product-list.vue'
 
 const app = new Vue({
   el: '#app',
   components: {
-    'product-list': ProductList
+    'product-list': ProductList,
+    'common-header': Header
   }
 })
---
+++ src/component/common/header.vue
@@ -0,0 +1,52 @@
+<template>
+  <header class="l-header" id="pagetop">
+    <div class="inner container">
+      <div class="row">
+        <h1 class="header-logo">
+          <a href="index.html">PORTFOLIO</a>
+        </h1>
+        <ul class="header-icon-sp">
+          <li class="sp-icon">
+            <a href="#" class="js-header-toggleBtn">
+              <!-- font-awesomeでアイコンを入れる -->
+            </a>
+          </li>
+        </ul>
+        <!--sp_icon_wrap-->
+        <nav class="header-sp-navi">
+          <ul class="inner">
+            <li class="sp-navi-list">
+              <a href="#" class="sp-navi-link">NAVI①</a>
+            </li>
+            <li class="sp-navi-list">
+              <a href="#" class="sp-navi-link">NAVI②</a>
+            </li>
+            <li class="sp-navi-list">
+              <a href="#" class="sp-navi-link">NAVI③</a>
+            </li>
+            <li class="sp-navi-list">
+              <a href="#" class="sp-navi-link">NAVI④</a>
+            </li>
+          </ul>
+        </nav>
+        <nav class="header-pc-navi">
+          <ul>
+            <li class="pc-navi-list">
+              <a href="#">NAVI①</a>
+            </li>
+            <li class="pc-navi-list">
+              <a href="#">NAVI②</a>
+            </li>
+            <li class="pc-navi-list">
+              <a href="#">NAVI③</a>
+            </li>
+            <li class="pc-navi-list">
+              <a href="#">NAVI④</a>
+            </li>
+          </ul>
+        </nav>
+        <!-- pc_navi -->
+      </div>
+    </div>
+  </header>
+</template>
