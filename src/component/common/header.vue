69593d83f7233398e2e7c16d57b9eae51f35912a
AtsushiTokuoka <atsushi.tokuoka@gmail.com>
2019-11-05 22:23:12 +0900

    header レイアウト調整

--- src/component/common/header.vue
+++ src/component/common/header.vue
@@ -1,52 +1,46 @@
 <template>
   <header class="l-header" id="pagetop">
     <div class="inner container">
-      <div class="row">
-        <h1 class="header-logo">
-          <a href="index.html">PORTFOLIO</a>
-        </h1>
-        <ul class="header-icon-sp">
-          <li class="sp-icon">
-            <a href="#" class="js-header-toggleBtn">
-              <!-- font-awesomeでアイコンを入れる -->
-            </a>
+      <h1 class="header-ttl">
+        <a href="index.html">PORTFOLIO</a>
+      </h1>
+      <p class="header-icon-sp">
+        <i class="fas fa-bars"></i>
+      </p>
+      <!--sp_icon_wrap-->
+      <nav class="header-sp-navi">
+        <ul class="inner">
+          <li class="sp-navi-list">
+            <a href="#" class="sp-navi-link">NAVI①</a>
+          </li>
+          <li class="sp-navi-list">
+            <a href="#" class="sp-navi-link">NAVI②</a>
+          </li>
+          <li class="sp-navi-list">
+            <a href="#" class="sp-navi-link">NAVI③</a>
+          </li>
+          <li class="sp-navi-list">
+            <a href="#" class="sp-navi-link">NAVI④</a>
           </li>
         </ul>
-        <!--sp_icon_wrap-->
-        <nav class="header-sp-navi">
-          <ul class="inner">
-            <li class="sp-navi-list">
-              <a href="#" class="sp-navi-link">NAVI①</a>
-            </li>
-            <li class="sp-navi-list">
-              <a href="#" class="sp-navi-link">NAVI②</a>
-            </li>
-            <li class="sp-navi-list">
-              <a href="#" class="sp-navi-link">NAVI③</a>
-            </li>
-            <li class="sp-navi-list">
-              <a href="#" class="sp-navi-link">NAVI④</a>
-            </li>
-          </ul>
-        </nav>
-        <nav class="header-pc-navi">
-          <ul>
-            <li class="pc-navi-list">
-              <a href="#">NAVI①</a>
-            </li>
-            <li class="pc-navi-list">
-              <a href="#">NAVI②</a>
-            </li>
-            <li class="pc-navi-list">
-              <a href="#">NAVI③</a>
-            </li>
-            <li class="pc-navi-list">
-              <a href="#">NAVI④</a>
-            </li>
-          </ul>
-        </nav>
-        <!-- pc_navi -->
-      </div>
+      </nav>
+      <nav class="header-pc-navi">
+        <ul>
+          <li class="pc-navi-list">
+            <a href="#">NAVI①</a>
+          </li>
+          <li class="pc-navi-list">
+            <a href="#">NAVI②</a>
+          </li>
+          <li class="pc-navi-list">
+            <a href="#">NAVI③</a>
+          </li>
+          <li class="pc-navi-list">
+            <a href="#">NAVI④</a>
+          </li>
+        </ul>
+      </nav>
+      <!-- pc_navi -->
     </div>
   </header>
 </template>
