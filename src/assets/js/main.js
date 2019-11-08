f712e4b045da56561f2181b8a9fc2ff1f1a3f846
AtsushiTokuoka <atsushi.tokuoka@gmail.com>
2019-11-08 22:59:09 +0900

    sidemenu テンプレート作成

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,12 @@
 import Header from '../../component/common/header.vue'
 import ProductList from '../../component/product-list.vue'
+import Sidemenu from '../../component/common/sidemenu.vue'
 
 const app = new Vue({
   el: '#app',
   components: {
     'product-list': ProductList,
-    'common-header': Header
+    'common-header': Header,
+    'side-menu': Sidemenu
   }
 })
