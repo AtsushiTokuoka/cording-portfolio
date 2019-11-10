d6a41ce10727aec2bdc1667fe26a068fca8b4a8c
AtsushiTokuoka <atsushi.tokuoka@gmail.com>
2019-11-10 12:22:31 +0900

    footerテンプレート作成

---
+++ src/component/common/footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <!--footer start-->
+  <footer class="l-footer mt-4">
+    <div class="inner container">
+      <ul class="footer-list-wrap">
+        <li class="footer-list">
+          <a href="#">footer-link1</a>
+        </li>
+        <li class="footer-list">
+          <span>・</span>
+          <a href="#">footer-link2</a>
+        </li>
+        <li class="footer-list">
+          <span>・</span>
+          <a href="#">footer-link3</a>
+        </li>
+      </ul>
+      <small class="copyright">copyright (C) PORTFOLIO all rights reserved.</small>
+    </div>
+  </footer>
+  <!--footer end-->
+</template>
+
+<script>
+export default {
+  name: 'common-footer',
+  data() {
+    return {}
+  }
+}
+</script>
