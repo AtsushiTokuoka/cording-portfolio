a235eaf9ccfc0e6a363c3066ef3f5abb56b62528
tokuoka <tokuoka@ivp.co.jp>
2019-11-11 17:28:32 +0900

    商品情報を外部ファイルから読み込み

--- src/component/product-list.vue
+++ src/component/product-list.vue
@@ -27,6 +27,7 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'product-list',
   computed: {
@@ -66,21 +67,14 @@ export default {
     return {
       sortOrder: 1,
       // 商品リスト
-      products: [
-        { id: 2, name: '商品タイトル②', price: 1200, image: 'item_02.jpg' },
-        { id: 1, name: '商品タイトル①', price: 1580, image: 'item_01.jpg' },
-        { id: 3, name: '商品タイトル③', price: 600, image: 'item_03.jpg' },
-        { id: 4, name: '商品タイトル④', price: 400, image: 'item_04.jpg' },
-        { id: 6, name: '商品タイトル⑥', price: 1000, image: 'item_06.jpg' },
-        { id: 5, name: '商品タイトル⑤', price: 1100, image: 'item_05.jpg' },
-        { id: 7, name: '商品タイトル⑦', price: 900, image: 'item_07.jpg' },
-        { id: 9, name: '商品タイトル⑨', price: 850, image: 'item_09.jpg' },
-        { id: 8, name: '商品タイトル⑧', price: 980, image: 'item_08.jpg' },
-        { id: 11, name: '商品タイトル⑪', price: 750, image: 'item_11.jpg' },
-        { id: 10, name: '商品タイトル⑩', price: 700, image: 'item_10.jpg' },
-        { id: 12, name: '商品タイトル⑫', price: 800, image: 'item_12.jpg' }
-      ]
+      products: []
     }
+  },
+  mounted: function() {
+    // 商品情報を取得
+    axios.get('/assets/product-data.json').then(response => {
+      this.products = response.data
+    })
   }
 }
 </script>
